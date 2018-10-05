6cce884475e38e64e9ae115d1bc65f32dac19ff3
Victor Jose Arana Rodriguez <victor.arana@walmart.com>
2018-10-05 09:09:17 -0500

    Pseudocode added.

--- script.js
+++ script.js
@@ -10,5 +10,12 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  addToDo();  
+}
+
+function addToDo(){
+  // 1. Create new li element.
+  // 1.1. Add text to the new li element.
+  // 2. Grab a reference to the todo-list.
+  // 3. Add the new element to the todo-list.
 }
