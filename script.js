5e716710a9b61816ecb3abce5852e7f4360bfd72
Victor Jose Arana Rodriguez <victor.arana@walmart.com>
2018-10-05 09:15:46 -0500

    addToDo function implemented.

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ function newTodo() {
 
 function addToDo(){
   // 1. Create new li element.
+  const toDo = document.createElement('li');
   // 1.1. Add text to the new li element.
+  toDo.textContent = 'New TODO element';
   // 2. Grab a reference to the todo-list.
   // 3. Add the new element to the todo-list.
+  list.appendChild(toDo);
 }
